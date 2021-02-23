85186a49d9ce3388d7aefe9304652393c6694ee8
ryo.kato <tenniskato6@gmail.com>
2021-02-23 17:09:06 +0900

    Add getAllTasks

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -8,7 +8,7 @@ public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME ="tasklist";
     private static EntityManagerFactory emf;
 
-    private static EntityManager createEntityManager(){
+    public static EntityManager createEntityManager(){
         return getEntityManagerFactory().createEntityManager();
     }
     private static EntityManagerFactory getEntityManagerFactory(){
